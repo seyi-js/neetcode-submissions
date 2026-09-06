27923044077d8e69bdf5405f7b3af9f5dae6c29f
Samuel Seyi <adebayosamueljahsmine30@gmail.com>
2026-09-05 23:44:29 -0700

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.ts
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums: number[]): number {
+        const numSet = new Set(nums);
+        let streak =0;
+
+
+        for (let num of nums){
+            
+            if(!numSet.has(num-1)){
+                //this is the start of a sequence/streak
+                let length = 1;
+
+                while(numSet.has(num + length)){
+                    length++;
+                }
+
+                if(length > streak){
+                    streak = length;
+                }
+
+                
+            }
+
+
+        }
+
+       
+
+
+        return streak;
+    }
+}
