1a64376c193c974adcfa4f8f0a6b28c879abf71e
Samuel Seyi <adebayosamueljahsmine30@gmail.com>
2026-09-07 12:14:08 -0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let maxProfit = 0;
+
+        for (let i = 0; i < prices.length; i++) {
+           
+            for (let j = i; j >= 0; j--) {
+                
+                const profit = prices[i] - prices[j];
+               
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+}
